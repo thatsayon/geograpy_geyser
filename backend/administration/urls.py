3b19ad68193c2db50c30a9c604adee237902bad9
Ashiqul Islam Ayon <ashiqulislamayon28@gmail.com>
2025-11-01 06:56:35 +0600

    question url updated

--- backend/administration/urls.py
+++ backend/administration/urls.py
@@ -3,9 +3,10 @@ from django.urls import path
 from module.views import (
     CreateModuleView,
     DeleteModuleView,
+    CreateQuestionView,
 )
 urlpatterns = [
     path('modules/', CreateModuleView.as_view(), name='Modules'),
-    path('modules/<uuid:id>/delete/', DeleteModuleView.as_view(), name='delete_module'),
-
+    path('modules/<uuid:id>/delete/', DeleteModuleView.as_view(), name='Delete Module'),
+    path('questions/', CreateQuestionView.as_view(), name='Questions'),
 ]
